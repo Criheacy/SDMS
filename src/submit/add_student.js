f27134e82f77ca59e42f7d42a0c06e8e4c76b74d
Criheacy <1052999141@qq.com>
2021-01-13 11:54:53 +0800

    Debug（未完成）

--- src/submit/add_student.js
+++ src/submit/add_student.js
@@ -1,30 +1,35 @@
-document.getElementById("add_student_submit").onclick = function()
+window.onload = function()
 {
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.open('POST', 'http://localhost:8080/', true);
-    httpRequest.setRequestHeader("Content-type", "application/json");
-    const obj =
+    document.getElementById("submitButton").onclick = function()
     {
-        "StuID" : document.getElementById("studentIDValue").value,
-        "StuName" : document.getElementById("studentNameValue").value,
-        "StuMajor" : document.getElementById("majorValue").value,
-        "StuGrade" : document.getElementById("gradeValue").value,
-        "StuClass" : document.getElementById("classValue").value,
-    };
-    alert(obj);
-    console.log(obj);
+        var stuID = document.getElementById("stuID").value.toString();
+		var stuName = document.getElementById("stuName").value.toString();
+		var stuMajor = document.getElementById("stuMajor").value.toString();
+		var stuGrade = document.getElementById("stuGrade").value.toString();
+		var stuClass = document.getElementById("stuClass").value.toString();
+        
+        var url = 'http://localhost:8080' + '/s/add_student?'
+				+ 'stuID=' + stuID + '&stuName=' + stuName
+                + '&stuMajor=' + stuMajor + '&stuGrade=' + stuGrade
+                + '&stuClass=' + stuClass;
+        
+		console.log(url);
 
-    httpRequest.send(JSON.stringify(obj));
-
-    alert(JSON.stringify(obj));
-    console.log(JSON.stringify(obj));
-
-    httpRequest.onreadystatechange = function ()
-    {
-        if (httpRequest.readyState == 4 && httpRequest.status == 200)
-        {
-            var json = httpRequest.responseText;
-            console.log(json);
-        }
-    };
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.setRequestHeader("Content-type", "application/json");
+        
+		httpRequest.open('GET', url, true);
+		httpRequest.send();
+		
+		httpRequest.onreadystatechange = function () {
+			if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+				var res = httpRequest.responseText;
+				console.log(res);
+				res = JSON.parse(res);
+				console.log(res.code);
+				if (res.code == 1)
+					alert("新学生信息添加成功！");
+			}
+		};
+    }
 }
